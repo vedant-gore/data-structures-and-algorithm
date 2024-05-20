5fad6735d6f35a253544cda9a7c533b5ce8b3438
vedant <vedantg.r10@gmail.com>
2024-05-20 17:46:37 +0530

    Binary Search Problem 02

--- sqrtx.java
+++ sqrtx.java
@@ -41,3 +41,25 @@ class Solution {
         System.out.println("\nSquare root of x is : " + sqroot);
     }
 }
+
+/*
+class Solution {
+    public int mySqrt(int x) {
+        int start = 1;
+        int end = x;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(mid > x/mid){
+                end = mid-1;
+            }
+            else if(mid < x/mid){
+                start = mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return end;
+    }
+}
+ */
