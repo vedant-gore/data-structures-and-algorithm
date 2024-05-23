1164118978906dc1310293716014f842007d6fbd
vedant <vedantg.r10@gmail.com>
2024-05-24 01:25:09 +0530

    Binary Search Problem 07

---
+++ searchInsertPosition.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            long mid = start + (end-start)/2;
+            if(target == nums[(int)mid]){
+                return (int)mid;
+            }
+            else if(target > nums[(int)mid]){
+                start = (int)mid+1;
+            }
+            else{
+                end = (int)mid-1;
+            }
+        }
+        return start;
+    }
+}
