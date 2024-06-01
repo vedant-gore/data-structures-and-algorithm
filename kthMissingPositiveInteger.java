c49977dfcb37af47011b277b4ae06dc8b53834b9
vedant <vedantg.r10@gmail.com>
2024-06-01 15:42:22 +0530

    Binary Search Problem 14

---
+++ kthMissingPositiveInteger.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int n = arr.length;
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] - (mid + 1) < k) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        
+        return l + k;
+    }
+}
