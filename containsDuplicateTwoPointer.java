9169cae8d61d1616d5a11a3af63d2100c084662f
Vedant Gore <144336661+vedant-gore@users.noreply.github.com>
2024-07-25 02:28:23 +0530

    Two Pointer Problem 04

---
+++ containsDuplicateTwoPointer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            int count=0;
+            for(int j=0; j<n; j++){
+                if(nums[i] == nums[j]) count++;
+            }
+            if(count>=2) return true;
+        }
+        return false;        
+    }
+}
