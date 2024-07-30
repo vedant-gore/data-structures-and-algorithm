b4a1034cf27b44ff0f8d54cdd7cd4a1f28932ffc
Vedant Gore <144336661+vedant-gore@users.noreply.github.com>
2024-07-30 23:26:13 +0530

    Create MAX3.java

---
+++ MAX3.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        if (nums.length == 3) return nums[0] * nums[1] * nums[2];
+        
+        Arrays.sort(nums);
+        
+        // Calculate the product of the three largest numbers
+        int max1 = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
+        
+        // Calculate the product of the two smallest numbers and the largest number
+        int max2 = nums[0] * nums[1] * nums[nums.length - 1];
+        
+        // Return the maximum of the two products
+        return Math.max(max1, max2);
+    }
+}
