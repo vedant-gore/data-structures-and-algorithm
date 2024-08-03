3b5bd7be29518749a259bc04ea52f988d21a788f
Vedant Gore <144336661+vedant-gore@users.noreply.github.com>
2024-08-03 12:08:05 +0530

    Counting/Frequency Arrays Method

---
+++ sortArrayByParityIIFrequencyArraysMethod.java
@@ -0,0 +1,27 @@
+class sortArrayByParityIIFrequencyArraysMethod {
+    public int[] sortArrayByParityII(int[] nums) {
+        int frequencyArr[] = new int[1001];
+
+        for (int num : nums) {
+            frequencyArr[num]++;
+        }
+
+        int i = 0;
+        for (int j = 0; j < 1001; j += 2) {
+            while (frequencyArr[j] > 0) {
+                nums[i] = j;
+                frequencyArr[j]--;
+                i += 2;
+            }
+        }
+        int k = 1;
+        for (int j = 1; j < 1001; j += 2) {
+            while (frequencyArr[j] > 0) {
+                nums[k] = j;
+                frequencyArr[j]--;
+                k += 2;
+            }
+        }
+        return nums;
+    }
+}
